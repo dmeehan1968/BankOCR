94d6dd6814f55df7467a4fd2d9db7c9271845439
Dave Meehan <dave_meehan@replicated.co.uk>
2013-04-26 23:32:55 +0100

    Implement digit recognition

--- BankOCRTests/OCRParserSpec.m
+++ BankOCRTests/OCRParserSpec.m
@@ -1,11 +1,13 @@
 #import "Kiwi.h"
 #import "OCRParser.h"
+#import "OCRDigit.h"
 
 SPEC_BEGIN(OCRParserSpec)
 
 describe(@"OCRParser", ^{
 	
     __block OCRParser *sut;
+	__block NSArray *inputStrings;
     
     beforeEach(^{
 		
@@ -23,6 +25,41 @@ describe(@"OCRParser", ^{
         [sut shouldNotBeNil];
         
     });
+	
+	it(@"should recognise all digits in input string", ^{
+		
+		inputStrings = @[	@"    _  _     _  _  _  _  _ ",
+							@"  | _| _||_||_ |_   ||_||_|",
+							@"  ||_  _|  | _||_|  ||_| _|",
+					];
+		
+		NSArray *digits = [sut digitsFromArrayOfStrings:inputStrings];
+		
+		[[digits should] haveCountOf:9];
+		
+		[digits enumerateObjectsUsingBlock:^(id<OCRDigit> obj, NSUInteger idx, BOOL *stop) {
+			
+			[[theValue(idx+1) should] equal: theValue([obj integerValue])];
+		}];
+	});
+	
+	it(@"should recognise all digits in input string (includes zero)", ^{
+		
+		inputStrings = @[	@" _     _  _     _  _  _  _ ",
+							@"| |  | _| _||_||_ |_   ||_|",
+							@"|_|  ||_  _|  | _||_|  ||_|",
+					];
+		
+		NSArray *digits = [sut digitsFromArrayOfStrings:inputStrings];
+		
+		[[digits should] haveCountOf:9];
+		
+		[digits enumerateObjectsUsingBlock:^(id<OCRDigit> obj, NSUInteger idx, BOOL *stop) {
+			
+			[[theValue(idx) should] equal: theValue([obj integerValue])];
+		}];
+	});
+	
     
 });
 
