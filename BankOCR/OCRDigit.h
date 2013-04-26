8e3e8b329e11dda3cbf3428446b734e59a5b106d
Dave Meehan <dave_meehan@replicated.co.uk>
2013-04-26 19:39:29 +0100

    Refactored confidence and integerValue as properties and moved matching into separate method only called when state is queried

--- BankOCR/OCRDigit.h
+++ BankOCR/OCRDigit.h
@@ -1,12 +1,15 @@
 
 @protocol OCRDigit <NSObject>
 
--(float)confidence;
+-(float) confidence;
 -(NSInteger)integerValue;
 
 @end
 
 @interface OCRDigit : NSObject <OCRDigit>
+
+@property (readonly, nonatomic) float confidence;
+@property (readonly, nonatomic) NSInteger integerValue;
 
 -(void) setSymbol: (char) symbol atRow: (NSInteger) row column: (NSInteger) column;
 
--- BankOCR/OCRDigit.m
+++ BankOCR/OCRDigit.m
@@ -3,6 +3,9 @@
 
 @interface OCRDigit ()
 
+@property (getter = isDirty) BOOL dirty;
+@property (readwrite, nonatomic) float confidence;
+@property (readwrite, nonatomic) NSInteger integerValue;
 
 @end
 
@@ -64,6 +67,17 @@ const char _digits[10][3][3] = {
 	char _matrix[3][3];
 }
 
+-(id)init {
+	
+	if (self = [super init]) {
+		_dirty = NO;
+		_confidence = 0.0;
+		_integerValue = -1;
+	}
+	return self;
+	
+}
+
 -(void) setSymbol: (char) symbol atRow: (NSInteger) row column: (NSInteger) column {
 	
 	NSAssert(row >= 0 && row <= 2, @"");
@@ -71,13 +85,36 @@ const char _digits[10][3][3] = {
 	
 	_matrix[row][column] = symbol;
 	
+	self.dirty = YES;
 }
 
 -(float)confidence {
 	
+	[self recalculate];
+		
+	return _confidence;
+}
+
+-(NSInteger)integerValue {
+	
+	[self recalculate];
+	
+	return _integerValue;
+	
+}
+
+-(void) recalculate {
+	
+	if (self.isDirty == NO) {
+		return;
+	}
+	
 	for (int digit=0; digit < 10; digit++) {
 		
 		int matches = 0;
+
+		self.confidence = 0.0;
+		self.integerValue = -1;
 		
 		for (int row=0; row < 3; row++) {
 			
@@ -90,17 +127,11 @@ const char _digits[10][3][3] = {
 		}
 		
 		if (matches == 9) {
-			return 1.0;
+			self.confidence = 1.0;
+			self.integerValue = digit;
+			return;
 		}
 	}
-	return 0.0;
-	
-}
-
--(NSInteger)integerValue {
-	
-	return 0;
-	
 }
 
 @end
